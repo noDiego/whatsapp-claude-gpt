0e8ca849dcf2ecffe060e6e0c5d198bac46dd5c3
diego-beltran-globalpay <diego.beltrangalvez@globalpay.com>
2025-08-27 21:03:34 -0400

    Update import statement to correctly load 'winston-daily-rotate-file' module without destructuring.

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,5 @@
 import * as winston from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
+import 'winston-daily-rotate-file';
 import { getFormattedDate } from './utils';
 
 const fileLogFormat = winston.format.printf((info) => {
