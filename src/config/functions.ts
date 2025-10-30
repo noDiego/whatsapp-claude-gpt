86ea18c8f0cc2089627a101cfa308935e44a7f79
diego-beltran-globalpay <diego.beltrangalvez@globalpay.com>
2025-10-30 13:59:59 -0300

    Fix function generate edit

--- src/config/functions.ts
+++ src/config/functions.ts
@@ -64,7 +64,7 @@ const generate_image = {
                 },
                 prompt: { type: "string", description: 'Description of the image to generate' },
                 background: { type: ["string","null"], enum: ["opaque","transparent","auto"], description: "Transparent or opaque background. OPTIONAL", nullable: true },
-                output_format: { type: ["string","null"], enum: ["png","jpg","webp"], description: "Default png. OPTIONAL", nullable: true },
+                output_format: { type: ["string","null"], enum: ["png","jpeg","webp"], description: "Default png. OPTIONAL", nullable: true },
                 quality: { type: ["string","null"], enum: ["medium","high" ,"auto"], description: "The quality of the image that will be generated. Default \"auto\". OPTIONAL", nullable: true },
                 send_as: {
                     type: ["string", "null"],
@@ -110,7 +110,7 @@ const generate_image_withedit = {
                     nullable: true
                 },
                 background: { type: ["string","null"], enum: ["opaque","transparent","auto"], description: "Transparent or opaque background. OPTIONAL", nullable: true },
-                output_format: { type: ["string","null"], enum: ["png","jpg","webp"], description: "Default png. OPTIONAL", nullable: true },
+                output_format: { type: ["string","null"], enum: ["png","jpeg","webp"], description: "Default png. OPTIONAL", nullable: true },
                 quality: { type: ["string","null"], enum: ["medium","high" ,"auto"], description: "The quality of the image that will be generated. Default \"auto\". OPTIONAL", nullable: true },
                 send_as: {
                     type: ["string", "null"],
@@ -119,7 +119,7 @@ const generate_image_withedit = {
                     nullable: true,
                     default: "image"
                 },
-                size: { type: ["string","null"], enum: ["1024x1024", "1536x1024", "1024x1536", "256x256", "512x512", "1792x1024", "1024x1792", "auto"], description: "The size of the generated images. Default \"auto\". OPTIONAL", nullable: true }
+                size: { type: ["string","null"], enum: ["1024x1024", "1536x1024", "1024x1536", "auto"], description: "The size of the generated images. Default \"auto\". OPTIONAL", nullable: true }
             },
             required: ["msg_id","chatId","prompt"],
             additionalProperties: false
